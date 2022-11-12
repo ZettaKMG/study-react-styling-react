9d46a26a543751191184128d9a9ecb9c181622f4
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-12 15:18:07 +0900

    이름 짓는 규칙, CSS Selector

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@
 // styled-components: 스타일을 JS에 내장시키는 방식으로 스타일을 작성함과 동시에 해당 스타일이 적용된 컴포넌트를 만들 수 있게 함.
 // 실습의 흐름
 // ㄴ 프로젝트 준비 > 일반 CSS 사용 > Sass 사용 > CSS Module 사용 > styled-components 사용
+
+// 가장 흔한 방식, 일반 CSS
+// CSS 클래스를 중복되지 않게 만드는 것이 가장 중요!
+// ㄴ 이름을 지을 때 특별한 규칙 사용
+// ㄴ CSS Selector 활용
+
+// 이름 짓는 규칙
+// 예시
+// BEM 네이밍: 이름 지을 때 해당 클래스가 어디에서 어떤 용도로 사용되는지 명확하게 작성하는 방식
+// ㄴ .card__title-primary
+
+// CSS Selector
+// 예시
+// .App .logo {
+//   animation: App-logo-spin infinite 20s linear;
+//   height: 40vmin;
+// }
+
 import logo from './logo.svg';
 import './App.css';
 
