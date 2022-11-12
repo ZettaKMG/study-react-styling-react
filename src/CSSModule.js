bb8a8aef75b4f223c0d10d9c406b648b050cd093
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-12 17:27:56 +0900

    Sass와 함께 사용하기

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -12,8 +12,24 @@
 // export default CSSModule;
 
 // CSS Module 사용 클래스가 2개 이상일 때
+// import React from 'react';
+// import styles from './CSSModule.module.css';
+// const CSSModule = () => {
+//     return (
+//         <div className={`${styles.wrapper} ${styles.inverted}`}>
+//             안녕하세요, 저는 <span className="something">CSS Module!</span>
+//         </div>
+//     );
+// };
+
+// export default CSSModule;
+
+// className={`${styles.wrapper} ${styles.inverted}`} ES6 문법 템플릿 리터럴 사용
+// className={[styles.wrapper, styles.inverted].join('')} 문법 템플릿 리터럴 미사용
+
+// Saas와 함께 사용하기
 import React from 'react';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 const CSSModule = () => {
     return (
         <div className={`${styles.wrapper} ${styles.inverted}`}>
@@ -23,6 +39,3 @@ const CSSModule = () => {
 };
 
 export default CSSModule;
-
-// className={`${styles.wrapper} ${styles.inverted}`} ES6 문법 템플릿 리터럴 사용
-// className={[styles.wrapper, styles.inverted].join('')} 문법 템플릿 리터럴 미사용
