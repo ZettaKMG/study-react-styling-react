89adc90e0240b63718b0c8019b2e3e7068be7d1f
ZettaKMG <99157643+ZettaKMG@users.noreply.github.com>
2022-11-12 16:37:36 +0900

    CSS Module 사용 클래스가 2개 이상일 때

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,11 +1,28 @@
+// CSS Module 사용 클래스가 1개일 때
+// import React from 'react';
+// import styles from './CSSModule.module.css';
+// const CSSModule = () => {
+//     return (
+//         <div className={styles.wrapper}>
+//             안녕하세요, 저는 <span className="something">CSS Module!</span>
+//         </div>
+//     );
+// };
+
+// export default CSSModule;
+
+// CSS Module 사용 클래스가 2개 이상일 때
 import React from 'react';
 import styles from './CSSModule.module.css';
 const CSSModule = () => {
     return (
-        <div className={styles.wrapper}>
+        <div className={`${styles.wrapper} ${styles.inverted}`}>
             안녕하세요, 저는 <span className="something">CSS Module!</span>
         </div>
     );
 };
 
 export default CSSModule;
+
+// className={`${styles.wrapper} ${styles.inverted}`} ES6 문법 템플릿 리터럴 사용
+// className={[styles.wrapper, styles.inverted].join('')} 문법 템플릿 리터럴 미사용
